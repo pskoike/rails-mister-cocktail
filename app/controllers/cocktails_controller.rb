10b754d11fc2d20b90d4b75911127559d16c11e0
Pedro Koike <ps.koike@gmail.com>
2017-04-13 12:44:11 -0300

    model sintax error correct

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action
+  before_action :set_cocktail, only: [:show]
   def index
     @cocktails = Cocktail.all
   end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,7 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail_id, presence: true, uniqueness: { scope: :cocktail_id, :ingredient_id }
+  validates :cocktail_id, presence: true
   validates :ingredient_id, presence: true
   validates :name, uniqueness: true, presence: true
 
